86369e2e68f09171843a60934d7fcc4c11dbd082
DanildoSilva <danny09spark@gmail.com>
2022-12-07 21:20:47 -0100

    add: skills add: portfolio add: footer

--- js/language.js
+++ js/language.js
@@ -13,6 +13,18 @@
   var aboutP2 = document.querySelector('.p-2')
   var aboutCardTitle = document.querySelector('.aboutCardTitle')
 
+  var skillsTitle = document.querySelector('.skills-title')
+  var skillsLabel = document.querySelector('.skills-label')
+  var skillsTitleOne = document.querySelector('.titleOne')
+  var skillsTitleTwo = document.querySelector('.titleTwo')
+  var skillsTitleThree = document.querySelector('.titleThree')
+  var skillsCaptionOne = document.querySelector('.captionOne')
+  var skillsCaptionTwo = document.querySelector('.captionTwo')
+  var skillsCaptionThree = document.querySelector('.captionThree')
+
+  var portfolioTitle = document.querySelector('.portfolio-title')
+  var expLink = document.querySelector('.expLink')
+
   function changeLang() {
     if (langChecked.checked) {
       // navbar
@@ -28,6 +40,19 @@
       aboutP1.textContent = "Meu nome é Danildo Silva, tenho 24 anos, atualmente sou Desenvolvedor Front-End, UI & UX Designer. Também me interesso por SEO e manipulação de imagens.";
       aboutP2.textContent = "Focado em entregar códigos e designs limpos, com boas práticas de design e estilização, utilizando linguagens e frameworks puros, sempre entregando uma experiência otimizada, com melhor performance.";
       aboutCardTitle.textContent = "Meu Nome"
+      // skills 
+      skillsLabel.textContent = "Habilidades"
+      skillsTitle.textContent = "Tenho experiência em"
+      skillsTitleOne.textContent = "Desenvolvimento Web"
+      skillsTitleTwo.textContent = "UI e UX Design"
+      skillsTitleThree.textContent = "Sites Responsivos"
+      skillsCaptionOne.textContent = "Planejamento, construção e manutenção de websites e aplicações web. Utilizando diversas tecnologias, como HTML, CSS, JavaScript e Frameworks."
+      skillsCaptionTwo.textContent = "Planejamento e design de interfaces para websites e aplicações web que proporcionem experiências significativas e agradáveis para o usuário."
+      skillsCaptionThree.textContent = "Criar páginas que se ajustam automaticamente ao tamanho da tela do visitante usando grids fluidos, gráficos e CSS media queries."
+      // portfolio
+      portfolioTitle.textContent = "Meus Melhores Trabalhos"
+      expLink.textContent = "Ver Mais"
+
     } else {
       // navbar english
       aboutLink.textContent = "About me";
@@ -42,5 +67,17 @@
       aboutP1.textContent = "My name is Danildo Silva, I'm 24 years old, I am currently a Front-End Developer, UI & UX Designer. I am also interested in SEO and image manipulation.";
       aboutP2.textContent = "Focused on deliver clean codes anddesigns, with good practices in design and stylization, using both pure languages and frameworks, always delivering an optimized experience, with better performance.";
       aboutCardTitle.textContent = "My name"
+      // skills 
+      skillsLabel.textContent = "Skills"
+      skillsTitle.textContent = "What I Am Great at"
+      skillsTitleOne.textContent = "Web Development"
+      skillsTitleTwo.textContent = "UI & UX Design"
+      skillsTitleThree.textContent = "Responsive Websites"
+      skillsCaptionOne.textContent = "Planning, building and maintaining websites and web applications. Using various thechmologies, like HTML, CSS, JavaScript and Frameworks."
+      skillsCaptionTwo.textContent = "Planning and designing interfaces for websites and web applications that provide meaningful and enjoyable experiences for user."
+      skillsCaptionThree.textContent = "Making pages adapt and automatically adjust to the size and orientation of the visitor's screen device using fluid grids, graphics, and CSS media queries"
+      // portfolio
+      portfolioTitle.textContent = "My Creative Works"
+      expLink.textContent = "Explore More"
     }
   }
